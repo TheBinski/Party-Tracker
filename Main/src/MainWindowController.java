cd4e709003d112a75448af460f6eeed985baa5b5
cjholmes <31835814+cjholmes@users.noreply.github.com>
2017-11-03 23:54:49 -0400

    UI

---
+++ Main/src/MainWindowController.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+public class MainWindowController implements Initializable{
+
+    @FXML
+    Label strengthMod, dexMod, conMod, intMod, wisMod, chaMod;
+
+    @FXML
+    protected void openHelpWindow(ActionEvent e) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("helpWindow.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("About");
+            stage.setScene(new Scene(root, 600, 450));
+            stage.show();
+    }
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+}
